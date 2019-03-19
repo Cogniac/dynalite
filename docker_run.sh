412a46477658f58fa7ec3d0419264ca5b4e76de1
Sandip Patel <sandip@cogniac.co>
2019-03-18 18:38:00 -0700

    setup env for cogenv to work

--- docker_run.sh
+++ docker_run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -it -v /Users/sandip:/root/sandip -e INSTANCE_FILE=/etc/config/instance_data.json -e AWS_SECRET_ACCESS_KEY=abcdedg -e AWS_ACCESS_KEY_ID=testabced cogniac/dynalite:tests sh
+docker run -it -v /Users/sandip:/root/sandip -e INSTANCE_FILE=/etc/config/instance_data.json -e COG_ONPREM=true -e AWS_SECRET_ACCESS_KEY=abcdedg -e AWS_ACCESS_KEY_ID=testabced cogniac/dynalite:tests sh
